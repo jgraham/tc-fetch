f429fb86b9bd5474064d72bcd9beb6c915ed0b34
James Graham <james@hoppipolla.co.uk>
2025-03-05 13:55:40 +0000

    Add Python typing

---
+++ python/tcfetch.pyi
@@ -0,0 +1,20 @@
+from typing import Optional
+
+class TaskDownloadData:
+    id: str
+    name: str
+    path: str
+    run_id: Optional[str]
+
+def check_complete(
+    branch: str, commit: str, taskcluster_base: Optional[str] = None
+) -> bool: ...
+def download_artifacts(
+    branch: str,
+    commit: str,
+    artifact_name: Optional[str] = None,
+    taskcluster_base: Optional[str] = None,
+    task_filters: Optional[str] = None,
+    check_complete: bool = False,
+    out_dir: Optional[str] = None,
+) -> list[TaskDownloadData]: ...
